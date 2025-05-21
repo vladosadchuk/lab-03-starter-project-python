d4ddb7c9429cac63e7461410630917c3e2b31068
vladosadchuk <osadchukvlad19@gmail.com>
2025-05-21 23:22:31 +0300

    add matrix api

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
 
-@router.get('')
+@router.post('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrices')
+def multiply() -> dict:
+    a = np.random.rand(10, 10).tolist()
+    b = np.random.rand(10, 10).tolist()
+    product = (np.matmul(a, b)).tolist()
+    return {"matrix_a": a, "matrix_b": b, "product": product}
